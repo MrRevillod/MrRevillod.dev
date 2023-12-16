874928216b5e12a70c22b34040b1add6d61a671d
MrRevillod <lrevillod2022@alu.uct.cl>
2023-12-16 18:46:14 -0300

    feat: refactoring/responsive styles

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -29,7 +29,7 @@ const ContactForm = () => {
     }
 
     return (
-        <form ref={form} method='POST' className="md:w-7/12 w-10/12">
+        <form ref={form} method='POST' className="md:w-7/12 w-11/12">
             <div className="mb-6">
                 <label htmlFor="name" className="mb-2 block text-base font-semibold">
                     Nombre completo
--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -2,25 +2,25 @@
 const pagesMap = [{
 
     pageName: "SmartParking",
-    imgSrc: "/smartparking/smartparking.png",
+    imgSrc: "/images/smartparking/page.webp",
     url: "/proyectos/smartparking",
     desc: "Aplicación de administración y manejo de estacionamientos en tiempo real.",
 },
 {
     pageName: "One Dark Minimal",
-    imgSrc: "/one-dark-minimal.webp",
+    imgSrc: "/images/onedark/theme.webp",
     url: "/proyectos/one-dark-minimal",
     desc: "Tema basado en One Dark Pro con un enfoque minimalista y limpio.",
 },
 {
     pageName: "Perfíl de Github",
-    imgSrc: "/github.webp",
+    imgSrc: "/images/github.webp",
     url: "https://github.com/MrRevillod",
     desc: "Aquí encontrarás repositorios y mi trabajo en proyectos de interés.",
 },
 {
     pageName: "Recursos",
-    imgSrc: "/code.webp",
+    imgSrc: "/images/code.webp",
     url: "/recursos",
     desc: "Articulos, guías y más.",
 }]
