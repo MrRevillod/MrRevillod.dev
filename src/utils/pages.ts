709c2c03f799e2eff29e892098f585cb4c953687
MrRevillod <lrevillod2022@alu.uct.cl>
2023-12-31 17:47:26 -0300

    update

--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -4,25 +4,26 @@ const pagesMap = [{
     pageName: "SmartParking",
     imgSrc: "/images/smartparking/page.webp",
     url: "/proyectos/smartparking",
-    desc: "Aplicación de administración y manejo de estacionamientos en tiempo real.",
+    desc: "Aplicación de administración y manejo de estacionamientos en tiempo real",
 },
 {
     pageName: "One Dark Minimal",
     imgSrc: "/images/onedark/theme.webp",
     url: "/proyectos/one-dark-minimal",
-    desc: "Tema basado en One Dark Pro con un enfoque minimalista y limpio.",
-},
-{
-    pageName: "Perfíl de Github",
-    imgSrc: "/images/github.webp",
-    url: "https://github.com/MrRevillod",
-    desc: "Aquí encontrarás repositorios y mi trabajo en proyectos de interés.",
+    desc: "Tema basado en One Dark Pro con un enfoque minimalista y limpio",
 },
 {
     pageName: "Recursos",
     imgSrc: "/images/code.webp",
     url: "/recursos",
     desc: "Articulos, guías y más.",
-}]
+},
+{
+    pageName: "Typescript Auth API",
+    imgSrc: "/images/ts-auth/ts-auth.webp",
+    url: "/proyectos/ts-auth",
+    desc: "Api Rest desarrollada con ExpressJS, Typescript y PrismaORM",
+},
+]
 
 export default pagesMap
